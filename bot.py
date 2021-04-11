fcdc8e089296e46e4d602bb5ad10fac552ba83bf
taekyun-quriously <taekyun@quriously.com>
2021-04-11 16:28:14 +0900

    discord bot and github webhook

---
+++ bot.py
@@ -0,0 +1,90 @@
+# import discord
+# from discord.ext import commands
+# app = commands.Bot(command_prefix='!')
+#
+# @app.event
+# async def on_ready():
+#     print(app.user.name, 'has connected to Discord!')
+#     await app.change_presence(status=discord.Status.online, activity=None)
+#     print("ready")
+#
+# @app.command()
+# async def 자비스(ctx, *, text):
+#     await ctx.send(text)
+#     app.run('ODMwNjc5MTA2MTA2Mjk0MzEy.YHKMNA.ubAyUOME-xHqYC5-gqhqiYuArrE')
+
+
+import discord
+from discord.ext import commands
+
+
+class MyClient(discord.Client):
+    hi_list = ['!gd', '!ㅎㅇ','!하이', '!hi']
+
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        if message.author.bot:
+            return None
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+        if message.content in self.hi_list:
+            channel = message.channel
+            msg = "ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇ"
+            await channel.send(msg)
+            return None
+
+
+client = MyClient()
+client.run('ODMwNjc5MTA2MTA2Mjk0MzEy.YHKMNA.ubAyUOME-xHqYC5-gqhqiYuArrE')
+
+# import os
+#
+# import discord
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+# TOKEN = os.getenv('ODMwNjc5MTA2MTA2Mjk0MzEy.YHKMNA.ubAyUOME-xHqYC5-gqhqiYuArrE')
+#
+# client = discord.Client()
+#
+# @client.event
+# async def on_ready():
+#     print(f'{client.user} has connected to Discord!')
+#
+# client.run(TOKEN)
+
+
+# import discord
+#
+# client = discord.Client()
+#
+# @client.event
+# async def on_ready():
+#     print('We have logged in as {0.user}'.format(client))
+#
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+#
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
+#
+# client.run('ODMwNjc5MTA2MTA2Mjk0MzEy.YHKMNA.jf-8YbkaOCodfIbBVmEE7LBjN-U')
+
+# import discord
+#
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print('Logged on as {0}!'.format(self.user))
+#
+#     async def on_message(self, message):
+#         print('Message from {0.author}: {0.content}'.format(message))
+#
+# client = MyClient()
+# client.run('ODMwNjc5MTA2MTA2Mjk0MzEy.YHKMNA.jf-8YbkaOCodfIbBVmEE7LBjN-U')
