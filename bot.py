51e6c31200b91176fe0f502d1376a7c0319b29ac
taekyun-quriously <taekyun@quriously.com>
2021-04-18 14:47:03 +0900

    chat copy

--- bot.py
+++ bot.py
@@ -23,53 +23,12 @@ class MyClient(discord.Client):
             await channel.send(msg)
             return None
 
+        if message.content:
+            channel = message.channel
+            msg = message.content
+            await channel.send(msg)
+            return None
+
 
 client = MyClient()
 client.run(SECRET['DISCORD_BOT_TOKEN'])
-
-# import os
-#
-# import discord
-# from dotenv import load_dotenv
-#
-# load_dotenv()
-# TOKEN = os.getenv('ODMwNjc5MTA2MTA2Mjk0MzEy.YHKMNA.ubAyUOME-xHqYC5-gqhqiYuArrE')
-#
-# client = discord.Client()
-#
-# @client.event
-# async def on_ready():
-#     print(f'{client.user} has connected to Discord!')
-#
-# client.run(TOKEN)
-
-
-# import discord
-#
-# client = discord.Client()
-#
-# @client.event
-# async def on_ready():
-#     print('We have logged in as {0.user}'.format(client))
-#
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-#
-# client.run('ODMwNjc5MTA2MTA2Mjk0MzEy.YHKMNA.jf-8YbkaOCodfIbBVmEE7LBjN-U')
-
-# import discord
-#
-# class MyClient(discord.Client):
-#     async def on_ready(self):
-#         print('Logged on as {0}!'.format(self.user))
-#
-#     async def on_message(self, message):
-#         print('Message from {0.author}: {0.content}'.format(message))
-#
-# client = MyClient()
-# client.run('ODMwNjc5MTA2MTA2Mjk0MzEy.YHKMNA.jf-8YbkaOCodfIbBVmEE7LBjN-U')
